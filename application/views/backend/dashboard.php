f45b5a4f6c0e388ab0441c499af4c25fc86b4a1c
José Pereira <jose.m.g.pereira@hotmail.com>
2020-01-13 16:15:01 +0000

    dashboard leave to leaves

--- application/views/backend/dashboard.php
+++ application/views/backend/dashboard.php
@@ -49,9 +49,9 @@
                                     <div class="round align-self-center round-info"><i class="ti-rocket"></i></div>
                                     <div class="m-l-10 align-self-center">
 										<?php if($this->session->userdata('user_type')=='EMPLOYEE'){ ?>
-											<a href="<?php echo base_url(); ?>leave/EmApplication" class="text-muted m-b-0">Ver Licença</a>
+											<a href="<?php echo base_url(); ?>leaves/EmApplication" class="text-muted m-b-0">Ver Licença</a>
 										<?php } else { ?>
-											<a href="<?php echo base_url(); ?>leave/Application" class="text-muted m-b-0">Ver Licença</a>
+											<a href="<?php echo base_url(); ?>leaves/Application" class="text-muted m-b-0">Ver Licença</a>
 										<?php } ?>
                                     </div>
                                 </div>
