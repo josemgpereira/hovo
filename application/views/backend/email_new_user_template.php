bbc065e9c96afbd679cab6fd7b737e5c7413b6f4
José Pereira <jose.m.g.pereira@hotmail.com>
2020-02-14 12:24:09 +0000

    modificado template novo utilizador

--- application/views/backend/email_new_user_template.php
+++ application/views/backend/email_new_user_template.php
@@ -5,23 +5,25 @@
 	<title>Hovo Comunicativo</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
-<style>
-	p.email {
-		Margin-top: 0;
-		color: #565656;
-		font-family: Georgia,serif;
-		font-size: 16px;
-		line-height: 50px;
-		Margin-bottom: 25px;
-	}
-</style>
 <body>
+<p style="font-size: 20px;">Os seus dados de acesso são os seguintes:</p>
 <div>
-	<p class="email">
-		<b>E-mail:</b> <?php echo $email; ?><br>
-		<b>Senha:</b> <?php echo $password; ?><br>
-		<b>URL:</b> <?php echo base_url(); ?>
-	</p>
+	<table style="width: 100%;border: 1px solid black;">
+		<tbody>
+		<tr>
+			<td style="width: 50%;text-align: left;border: 1px solid black;font-size: 20px;"><b>URL:</td>
+			<td style="width: 50%;text-align: left;border: 1px solid black;font-size: 20px;"><?php echo base_url(); ?></td>
+		</tr>
+		<tr>
+			<td style="width: 50%;text-align: left;border: 1px solid black;font-size: 20px;"><b>E-mail:</td>
+			<td style="width: 50%;text-align: left;border: 1px solid black;font-size: 20px;"><?php echo $email; ?></td>
+		</tr>
+		<tr>
+			<td style="width: 50%;text-align: left;border: 1px solid black;font-size: 20px;"><b>Senha:</td>
+			<td style="width: 50%;text-align: left;border: 1px solid black;font-size: 20px;"><?php echo $password; ?></td>
+		</tr>
+		</tbody>
+	</table>
 </div>
 </body>
 </html>
